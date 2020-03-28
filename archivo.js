ae7a2cf3a065b3fb1fac52a9a0dc943ff5aea117
Christian-Cell <cristohp74@gmail.com>
2020-03-28 22:10:02 +0100

    practica de matrices java script

---
+++ archivo.js
@@ -0,0 +1,22 @@
+
+let names = [["joeCarter","ryanGarcia"],[1,3,5,7],["juanMiguel","joaoCarrera"]];
+alert(names.length);
+console.log(names.length);
+
+alert(names[1].length);
+console.log(names[1]);
+
+alert(names[2].length);
+console.log(names[2].length);
+
+alert(names[2][1].length);
+console.log(names[2][1].length);
+
+
+
+
+
+
+
+
+
